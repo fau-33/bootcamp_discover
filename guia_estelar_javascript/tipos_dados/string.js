73c1f145a4e65e8bf863cda0f2325b36be2ebd5f
Flávio Félix <fau_felix30@hotmail.com>
2021-02-02 17:07:15 -0300

    Terminando o guia estelar HTML e i e começando JS

---
+++ guia_estelar_javascript/tipos_dados/string.js
@@ -0,0 +1,13 @@
+/*
+
+  String
+  * Cadeia de caracteres
+  "" // aspas duplas
+  '' // aspas simples
+  `` // template literals ou template strings
+
+ */
+
+ console.log("Leandro")
+ console.log('Leandro')
+ console.log(`Flávio Leandro`)
